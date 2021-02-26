bb102bbfada2a774d995b6e0e7bc6f01d5995073
Luca Bacchi <bacchilu@gmail.com>
2021-02-26 08:25:45 +0000

    Draft for a working modal

--- src/navbar/index.js
+++ src/navbar/index.js
@@ -1,81 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import {Search} from './search.js';
-
-const AuthForm = function ({isVisible}) {
-    const inputEl = React.useRef(null);
-    React.useEffect(
-        function () {
-            inputEl.current.focus();
-        },
-        [isVisible]
-    );
-
-    const onSubmit = function (e) {
-        e.preventDefault();
-        console.log('SUBMIT');
-    };
-
-    return (
-        <form onSubmit={onSubmit}>
-            <div className="modal-body">
-                <div className="form-floating mb-3">
-                    <input ref={inputEl} type="email" className="form-control" placeholder="Username" />
-                    <label>Username</label>
-                </div>
-                <div className="form-floating">
-                    <input type="password" className="form-control" placeholder="Password" />
-                    <label>Password</label>
-                </div>
-            </div>
-            <div className="modal-footer">
-                <button type="submit" className="btn btn-primary">
-                    Enter
-                </button>
-            </div>
-        </form>
-    );
-};
-
-const Modal = function ({user, opened, hide, Body, children}) {
-    // https://stackoverflow.com/questions/32370994/how-to-pass-props-to-this-props-children
-    const modalEl = React.useRef(null);
-    const modalJs = React.useRef(null);
-    const [isVisible, setIsVisible] = React.useState(false);
-    React.useEffect(function () {
-        modalJs.current = new bootstrap.Modal(modalEl.current, {});
-        modalEl.current.addEventListener('hidden.bs.modal', function (event) {
-            hide();
-            setIsVisible(false);
-        });
-        modalEl.current.addEventListener('shown.bs.modal', function () {
-            setIsVisible(true);
-        });
-    }, []);
-    React.useEffect(
-        function () {
-            console.log(user);
-            opened ? modalJs.current.show() : modalJs.current.hide();
-        },
-        [opened]
-    );
-
-    return ReactDOM.createPortal(
-        <div ref={modalEl} className="modal fade" tabIndex="-1" aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title">Authentication</h5>
-                        <button className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <Body isVisible={isVisible} />
-                </div>
-            </div>
-        </div>,
-        document.getElementById('modal')
-    );
-};
+import {Modal, AuthForm} from './auth_modal.js';
 
 export const NavBar = function ({categories, user}) {
     // React.useEffect(function () {
@@ -127,8 +53,8 @@ export const NavBar = function ({categories, user}) {
     });
     return (
         <React.Fragment>
-            <Modal user={user} opened={modalOpened} hide={hideModal} Body={AuthForm}>
-                <AuthForm />
+            <Modal opened={modalOpened} hide={hideModal}>
+                <AuthForm user={user} />
             </Modal>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
