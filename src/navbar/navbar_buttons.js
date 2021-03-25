7b527b9d4289b25bcd01ad9a84ba01693fdfafe6
Luca Bacchi <bacchilu@gmail.com>
2021-03-25 08:33:25 +0000

    Auth button

--- src/navbar/navbar_buttons.js
+++ src/navbar/navbar_buttons.js
@@ -5,13 +5,13 @@ import {Modal} from './modal.js';
 import {AuthForm} from './auth_form.js';
 import {useUser} from '../user-context.js';
 
-const LoginButton = function ({onLogin}) {
+const LoginButton = function ({onUser}) {
     const [user, setUser] = useUser();
 
     if (user === undefined)
         return (
             <a className="nav-link disabled" href="#">
-                <button type="button" className="btn btn-light" onClick={onLogin}>
+                <button type="button" className="btn btn-light">
                     Login <i className="bi bi-box-arrow-in-right"></i>
                 </button>
             </a>
@@ -19,15 +19,17 @@ const LoginButton = function ({onLogin}) {
     if (user === null)
         return (
             <a className="nav-link" href="#">
-                <button type="button" className="btn btn-light" onClick={onLogin}>
+                <button type="button" className="btn btn-light" onClick={onUser}>
                     Login <i className="bi bi-box-arrow-in-right"></i>
                 </button>
             </a>
         );
     return (
         <a className="nav-link" href="#">
-            <button type="button" className="btn btn-light" onClick={onLogin}>
-                Login <i className="bi bi-box-arrow-in-right"></i>
+            <button type="button" className="btn btn-light" onClick={onUser}>
+                <strong>
+                    {user.login} <i className="bi bi-person"></i>
+                </strong>
             </button>
         </a>
     );
@@ -37,7 +39,7 @@ export const NavBarButtons = function (props) {
     const [user, setUser] = useUser();
     const [modalOpened, setModalOpened] = React.useState(false);
 
-    const onLogin = function (e) {
+    const onUser = function (e) {
         e.preventDefault();
         setModalOpened(true);
     };
@@ -63,7 +65,7 @@ export const NavBarButtons = function (props) {
                     <div className="navbar-collapse collapse" id="navbarCollapse">
                         <ul className="navbar-nav flex-row flex-wrap me-auto mb-2 mb-md-0">
                             <li className="nav-item col-6 col-lg-auto ms-auto">
-                                <LoginButton onLogin={onLogin} />
+                                <LoginButton onUser={onUser} />
                             </li>
                             <li className="nav-item col-6 col-lg-auto">
                                 <a className="nav-link" href="#">
