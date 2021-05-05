a37e0963c21688f16a1c72bfe2ff7da4adf6e9dc
Luca Bacchi <bacchilu@gmail.com>
2021-05-05 07:58:52 +0000

    Showing prices in products list

--- src/category.js
+++ src/category.js
@@ -6,9 +6,50 @@ import {TreeMenu} from './tree_menu.js';
 import {API} from './parameters.js';
 import {useUser} from './auth.js';
 
+const CurrencyFormatter = new Intl.NumberFormat('it-IT', {
+    style: 'currency',
+    currency: 'EUR',
+});
+
 const useProducts = function (id, user) {
     const baseUrl = `//${API}/products/level_3/${id}`;
-    return useSWR(user === null ? baseUrl : `${baseUrl}?jwt=${user.jwt}`);
+    return useSWR(function () {
+        return user === null ? baseUrl : `${baseUrl}?jwt=${user.jwt}`;
+    });
+};
+
+const Price = function ({value}) {
+    return (
+        <strong>
+            <em>{value === null ? null : CurrencyFormatter.format(value.price)}</em>
+        </strong>
+    );
+};
+
+const ProductRow = function ({item}) {
+    return (
+        <div className="card mb-1">
+            <div className="row g-0">
+                <div className="col-md-2 text-center align-middle">
+                    <img src={item.photos[0]} className="img-fluid" style={{maxHeight: '100px'}} />
+                </div>
+                <div className="col-md-10">
+                    <div className="card-body">
+                        <h5 className="card-title">{item.code_simple}</h5>
+                        <p className="card-text">{item.title.en}</p>
+                        <p className="card-text">
+                            {(item.stock === 0 && <small className="text-danger">not available</small>) || (
+                                <small className="text-muted">{item.stock} available</small>
+                            )}
+                            <span className="float-end">
+                                <Price value={item.price} />
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 const Subcategory = function (props) {
@@ -31,26 +72,7 @@ const Subcategory = function (props) {
         );
 
     const items = data.map(function (item) {
-        return (
-            <div key={item.id} className="card mb-1">
-                <div className="row g-0">
-                    <div className="col-md-2 text-center align-middle">
-                        <img src={item.photos[0]} className="img-fluid" style={{maxHeight: '100px'}} />
-                    </div>
-                    <div className="col-md-10">
-                        <div className="card-body">
-                            <h5 className="card-title">{item.code_simple}</h5>
-                            <p className="card-text">{item.title.en}</p>
-                            <p className="card-text">
-                                {(item.stock === 0 && <small className="text-danger">not available</small>) || (
-                                    <small className="text-muted">{item.stock} available</small>
-                                )}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
+        return <ProductRow key={item.id} item={item} />;
     });
     return (
         <React.Fragment>
