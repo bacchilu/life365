5012da42d4411c71829be93c1a54935d53d50947
Luca Bacchi <bacchilu@gmail.com>
2021-05-11 08:18:10 +0000

    Better listing page

--- src/category/product.js
+++ src/category/product.js
@@ -12,18 +12,9 @@ const Price = function ({value}) {
     );
 };
 
-export const ProductRow = function ({item}) {
-    const {data: user} = useUser();
-    const {data: cart} = useCart(user);
-
-    const inCart = cart.items
-        .map(function (item) {
-            return item.id;
-        })
-        .includes(item.id);
-
+const ProductRowNotAuthenticated = function ({item}) {
     return (
-        <div className={`card mb-1 ${inCart ? 'border-dark' : ''}`}>
+        <div className="card mb-1">
             <div className="row g-0">
                 <div className="col-md-2 text-center align-middle">
                     <img src={item.photos[0]} className="img-fluid" style={{maxHeight: '100px'}} />
@@ -31,6 +22,53 @@ export const ProductRow = function ({item}) {
                 <div className="col-md-10">
                     <div className="card-body">
                         <h5 className="card-title">{item.code_simple}</h5>
+                        <p className="card-text">{item.title.en}</p>
+                        <p className="card-text">
+                            {(item.stock === 0 && <small className="text-danger">not available</small>) || (
+                                <small className="text-muted">{item.stock} available</small>
+                            )}
+                            <span className="float-end">
+                                <Price value={item.price} />
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export const ProductRow = function ({item}) {
+    const {data: user} = useUser();
+    const {data: cart} = useCart(user);
+
+    if (cart === undefined || cart === null) return <ProductRowNotAuthenticated item={item} />;
+
+    const cartProduct = cart.items.find(function (e) {
+        return e.id === item.id;
+    });
+
+    return (
+        <div className={`card mb-1 ${cartProduct !== undefined ? 'border-dark' : ''}`}>
+            <div className="row g-0">
+                <div className="col-md-2 text-center align-middle">
+                    <img src={item.photos[0]} className="img-fluid" style={{maxHeight: '100px'}} />
+                </div>
+                <div className="col-md-10">
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col">
+                                <h5 className="card-title">{item.code_simple}</h5>
+                            </div>
+                            <div className="col">
+                                <span className="float-end">
+                                    {cartProduct !== undefined && <em>{cartProduct.qta}</em>}{' '}
+                                    <button type="button" className="btn btn-primary btn-sm">
+                                        <i className="bi bi-cloud-plus"></i>
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
                         <p className="card-text">{item.title.en}</p>
                         <p className="card-text">
                             {(item.stock === 0 && <small className="text-danger">not available</small>) || (
