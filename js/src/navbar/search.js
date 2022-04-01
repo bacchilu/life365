5261c2b629932dd1fd09e74ee5ba18c0609eed2d
Luca Bacchi <bacchilu@gmail.com>
2022-04-01 15:51:39 +0200

    Refactoring

--- js/src/navbar/search.js
+++ js/src/navbar/search.js
@@ -14,7 +14,7 @@ const useProducts = function (value) {
         }
     );
     return data === undefined
-        ? []
+        ? undefined
         : data.results.map(function (item) {
               return {id: item.id, code: item.code};
           });
@@ -23,14 +23,38 @@ const useProducts = function (value) {
 export const Search = function () {
     const [value, setValue] = React.useState('');
     const suggestions = useProducts(value.trim());
+    // const [matchedProduct, setMatchedProduct] = React.useState(null);
+    // React.useEffect(
+    //     function () {
+    //         if (suggestions === undefined) {
+    //             setMatchedProduct(null);
+    //             return;
+    //         }
+    //         const res = suggestions.find(function (item) {
+    //             return item.code.toLowerCase().trim() === value.toLowerCase().trim();
+    //         });
+    //         setMatchedProduct(res === undefined ? null : res);
+    //     },
+    //     [value, suggestions]
+    // );
+    // React.useEffect(
+    //     function () {
+    //         console.log(matchedProduct);
+    //     },
+    //     [matchedProduct]
+    // );
+    // console.log(matchedProduct);
 
     const onChange = function (e) {
         setValue(e.target.value);
     };
 
-    const options = suggestions.map(function (item) {
-        return <option key={item['id']} value={item['code']} />;
-    });
+    const options =
+        suggestions === undefined
+            ? null
+            : suggestions.map(function (item) {
+                  return <option key={item.id} value={item.code} />;
+              });
     return (
         <form className="d-flex">
             <input
